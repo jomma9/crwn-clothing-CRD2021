14b63c4493893e29e41914486266acef92c34c58
jomma9 <stephenjomma@gmail.com>
2020-12-08 14:58:45 -0600

    collection preview

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.componet.jsx';
-
-const HatsPage = (props) => {
-  debugger;
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
